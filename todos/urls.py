0bd3f17b409bb458792a30a5f4c5fb1e4ef4e4a1
Hikkari <gosha.troncraft@gmail.com>
2021-06-30 19:59:34 +0600

    Добавил папку static, переместил шаблоны и добавил их

--- todos/urls.py
+++ todos/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-	path('todo_list', todo_list, name = "todo_list"),
+	path('', todo_list, name = "todo_list"),
 	path('todo_list/todo/<int:todo_id>/', todo_detail, name = "todo_detail"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -3,6 +3,7 @@ from .models import *
 
 def todo_list(request):
 	todos = Todo.objects.all()
+	# categories = Category.objects.all()
 	context = {'todos':todos}
 	return render(request, 'todolist/todo_list.html', context)
 
@@ -11,3 +12,9 @@ def todo_detail(request, todo_id):
 	todo = Todo.objects.get(id = todo_id)
 	context = {'todo':todo}
 	return render(request, 'todolist/todo_detail.html', context)
+
+def category(request):
+	categories = Category.objects.all()
+	context = {'categories':categories}
+	return render(request, 'todolist/category_list.html')
+
