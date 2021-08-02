9f2e170655e9bc43652717f0c7ddea718f2b675a
HikkariWest <gosha.troncraft@gmail.com>
2021-08-02 17:46:03 +0600

    Исправил одну ошибку с отображением Премиум статуса и сделал главной странице список задач

--- todos/urls.py
+++ todos/urls.py
@@ -7,7 +7,7 @@ from .forms import *
 
 
 urlpatterns = [
-	path('todo_list/', todo_list, name = "todo_list"),
+	path('', todo_list, name = "todo_list"),
 	path('todo/<int:todo_id>/change_status/', change_todo_status, name = "change_todo_status"),
 	path('todo_list/todo/<int:todo_id>/', todo_detail, name = "todo_detail"),
 	path('categories/', category_list, name = "category_list"),
